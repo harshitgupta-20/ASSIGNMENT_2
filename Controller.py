df99218b0dd7c9e5cb01718c4215bcc9edc60e8a
harshitgupta-20 <Harshit.Gupta@ust.trumio.ai>
2026-01-09 13:53:08 +0530

    Create Controller.py

---
+++ Controller.py
@@ -0,0 +1,41 @@
+from fastapi import  HTTPException, APIRouter
+from Customer import Customer
+from service import CustomerService
+app = APIRouter(
+    prefix="/customers",
+    tags=["customers"],
+)
+
+customer_service = CustomerService()
+
+@app.post("/", status_code=201)
+def create_customer(customer: Customer):
+    return customer_service.add_customer(customer)
+
+@app.get("/",)
+def read_customers():
+    return customer_service.get_all_customers()
+
+@app.get("/{customer_id}")
+def read_customer(customer_id: int):
+    customer = customer_service.get_customer(customer_id)
+    if not customer:
+        raise HTTPException(status_code=404, detail="Customer not found")
+    return customer
+
+@app.put("/{customer_id}")
+def update_customer(customer_id: int, customer: Customer):
+    if customer_id != customer.id:
+        raise HTTPException(status_code=400, detail="ID mismatch")
+    
+    success = customer_service.update_customer(customer)
+    if not success:
+        raise HTTPException(status_code=404, detail="Customer not found")
+    return {"message": "Updated successfully"}
+
+@app.delete("/{customer_id}")
+def delete_customer(customer_id: int):
+    success = customer_service.remove_customer(customer_id)
+    if not success:
+        raise HTTPException(status_code=404, detail="Customer not found")
+    return {"message": "Deleted successfully"}
